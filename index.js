b8e1a69a271005cba3225507548b8555bc94850f
Saravanan-sar <saravanannagappan2004@gmail.com>
2024-11-03 20:53:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var sidenav = document.querySelector(".side-navbar");
+
+function showNavbar()
+{
+    sidenav.classList.add("show");
+}
+
+function closeNavbar()
+{
+    sidenav.classList.remove("show")
+}
